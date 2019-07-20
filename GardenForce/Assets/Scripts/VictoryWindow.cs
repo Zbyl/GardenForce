a442e1077fc4a8a7947681d75914df5bf354fa9d
Michał Kędzierski <ganondorf@wp.pl>
2019-07-20 22:14:59 +0200

    Added tick counter

--- GardenForce/Assets/Scripts/VictoryWindow.cs
+++ GardenForce/Assets/Scripts/VictoryWindow.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VictoryScreen : MonoBehaviour
+public class VictoryWindow : MonoBehaviour
 {
     readonly Color32[] textColors = {
-        new Color(62, 204, 217),
-        new Color(217, 140, 62),
+        new Color32(224, 214, 184, 255),
+        new Color32(217, 140, 62, 255),
+        new Color32(62, 204, 217, 255),
     };
     readonly string[] victoryTexts = {
-        "PLAER ORANGE WON!",
-        "PLAYER CYAN WON!"
+        "TIE!",
+        "PLAYER RED WON!",
+        "PLAYER BLUE WON!"
     };
     public void SetWinner(int winner)
     {
         var text = GetComponentInChildren<Text>();
         text.color = textColors[winner];
         text.text = victoryTexts[winner];
+        gameObject.SetActive(true);
     }
 }
